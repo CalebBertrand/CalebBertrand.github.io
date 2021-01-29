a980ff6f45a0a238e60e52429bab64b84c23b1ed
CalebBertrand <caleb.a.bertrand@gmail.com>
2021-01-29 12:55:53 -0600

    Moved caurosel buttons closer and made slides smaller

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import { main } from './index';
 
 import '../sass/index.scss';
+
 import { Core, Sticky, SmoothScroll, Triggers, MediaQuery, Tooltip } from 'foundation-sites/js/foundation.js';
 
 SmoothScroll.defaults.animationDuration = 800;
